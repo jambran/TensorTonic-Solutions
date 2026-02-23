82d7d30729e98b45b864ee02899d667133a73bca
Jamie Brandon <jbrandon@atlassian.com>
2026-02-23 14:19:57 -0500

    Update solution for logistic-regression-training

--- logistic-regression-training/logistic-regression-training.py
+++ logistic-regression-training/logistic-regression-training.py
@@ -15,33 +15,27 @@ def train_logistic_regression(X, y, lr=0.1, steps=1000):
     Train logistic regression via gradient descent.
     Return (w, b).
     """
-    X = np.array(X)  # shape (num_samples, num_features)
-    y = np.array(y)  # shape (num_samples)
-    num_training_samples = len(X)
-    num_features = len(X[0])
+    X = np.array(X)
+    y = np.array(y)
 
-    weights = np.zeros(num_features)  # shape (num_features)
-    bias = 0.0
+    num_training_samples, num_features = X.shape
+    weights = np.zeros(num_features)
+    bias = 0
 
     for step in range(steps):
-        predictions = _sigmoid(np.matmul(X, weights) + bias)
 
-        # model loss at this step
-        # loss = -1 / num_training_samples * sum(
-        #     [
-        #         gold_label * np.log(prediction) + (1 - gold_label) * np.log(1 - prediction)
-        #         for prediction, gold_label in zip(predictions, y)
-        #     ]
-        # )
+        # inference
+        probabilities = _sigmoid(
+            np.matmul(X, weights) + bias
+        )
 
-        weights_gradient = np.matmul(
-            X.transpose(),
-            (predictions - y),
-        ) / num_training_samples
-        biases_gradient = np.mean(predictions - y)
+        # update weights
+        difference = probabilities - y
+        gradient_w = np.matmul(X.transpose(), difference) / num_training_samples
+        gradient_b = np.mean(difference)
 
-        # adjust weights
-        weights = weights - lr * weights_gradient
-        bias = bias - lr * biases_gradient
+        weights = weights - lr * gradient_w
+        bias = bias - lr * gradient_b
+
 
     return weights, bias
